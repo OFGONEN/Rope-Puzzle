2f3a572cc9ea86db8b135d187ac213dc910e08a8
ofgonen_office <ofgonen@gmail.com>
2022-11-28 12:42:50 +0300

    🔃 SharedInput_JoyStick: Refactor

--- Assets/Script/FFStudio/Data/SharedInput_JoyStick.cs
+++ Assets/Script/FFStudio/Data/SharedInput_JoyStick.cs
@@ -1,57 +1,41 @@
 /* Created by and for usage of FF Studios (2021). */
 
 using UnityEngine;
+using Lean.Touch;
+using Sirenix.OdinInspector;
 
 namespace FFStudio
 {
 	[ CreateAssetMenu( fileName = "notif_input_joystick", menuName = "FF/Data/Shared/Input/JoyStick" ) ]
 	public class SharedInput_JoyStick : SharedVector2Notifier
 	{
-#region Fields (Public)
-		public SafeEvent input_toggle; 
-		
-		Vector2 input_received;
-		Vector2 input_screen_position;
-		Vector2 input_direction;
-		bool input_enabled;
+#region Fields
+		[ ShowInInspector, ReadOnly ] Vector2 finger_position;
+		[ ShowInInspector, ReadOnly ] Vector2 finger_direction;
+
+		public Vector2 FingerPosition  => finger_position;
+		public Vector2 FingerDirection => finger_direction;
 #endregion	
 
 #region Properties
-		public Vector2 Input_Received        => input_received;
-		public Vector2 Input_Screen_Position => input_screen_position;
-		public Vector2 Input_Direction       => input_direction;
-		public bool Input_Enabled            => input_enabled;
 #endregion
 
 #region API
-		public void Enable( Vector2 screenPosition )
+		public void OnFingerDown( LeanFinger leanFinger )
 		{
-			input_screen_position = screenPosition;
-			input_enabled         = true;
-
-			input_toggle?.Invoke();
+			finger_position = leanFinger.ScreenPosition;
 		}
 
-		public void Disable( Vector2 screenPosition )
+		public void OnFingerUp( LeanFinger leanFinger )
 		{
-			input_screen_position = screenPosition;
-			input_enabled         = false;
-			SharedValue           = Vector2.zero;
-
-			input_toggle?.Invoke();
+			finger_position = leanFinger.ScreenPosition;
 		}
 
-		public void ReceiveInput( Vector2 input )
+		public void OnFingerUpdate( LeanFinger leanFinger )
 		{
-			input_received  = input;
-			input_direction = input_received - input_screen_position;
-			SharedValue     = input_direction.normalized;
+			finger_direction = leanFinger.ScreenPosition - finger_position;
+			SharedValue      = finger_direction.normalized;
 		}
-
-		public void ClearInvokeList()
-		{
-			input_toggle = null;
-		}
-	#endregion
+#endregion
 }
 }
